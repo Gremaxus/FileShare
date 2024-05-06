e2eaf5986670a96fccb20c62267fa2d5ef3bddb9
Gremaxus <thomas.and.mercer@gmail.com>
2024-05-06 12:26:32 -0400

    Added multiple npm packages (express, websocket) and created a public directory to store html and css files as well. Began work on the server file, currently trying to get the express module to run properly so I can run the server for testing html files on a browser.

---
+++ FileSharingServerApplication/server.js
@@ -0,0 +1,36 @@
+const WebSocket = require('ws');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+// Serving up static files from the public directory
+app.use(express.static('public'));
+
+app.list(port, () => {
+   console.log(`Server running on http://localhost:${port}`);
+});
+
+wss.on('connection', function connection(ws) {
+   console.log('A new connection has been established.');
+
+   // For processing incoming messages
+   ws.on('message', function incoming(message) {
+      console.log('received %s', message);
+
+      // Broadcasting message while also excluding the sender
+      wss.clients.forEach(function each(client) {
+         if (client !== ws && client.readyState === WebSocket.OPEN) {
+            client.send(message);
+         }
+      });
+   });
+
+   // Disconnecting
+   ws.on('close', () => {
+      console.log('Client has disconnected.');
+   });
+});
+
+console.log('Signaling server running on ws://localhost:8080');
